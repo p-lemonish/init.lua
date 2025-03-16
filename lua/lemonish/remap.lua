b7e6d74c6e62dc47f24aa80c7d0b87bbf66d3ccb
Lemonish <patrikmihelson@gmail.com>
2025-03-16 10:53:55 +0200

    added some new remaps

--- lua/lemonish/remap.lua
+++ lua/lemonish/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>dv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>.', vim.lsp.buf.code_action, { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+yy', { noremap = true })
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+yy', { noremap = true })
