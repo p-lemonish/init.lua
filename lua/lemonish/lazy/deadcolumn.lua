07608d4f0ab5b998506bb4fa62a08789f4b05951
Lemonish <patrikmihelson@gmail.com>
2025-04-14 21:20:13 +0300

    enabled colorcolumn and installed deadcolumn

---
+++ lua/lemonish/lazy/deadcolumn.lua
@@ -0,0 +1,30 @@
+return {
+    'Bekaboo/deadcolumn.nvim',
+    config = function()
+        local opts = {
+            scope = 'line',
+            ---@type string|fun(): integer
+            ---@type string[]|boolean|fun(mode: string): boolean
+            modes = function(mode)
+                return mode:find('^[iRss\x13]') ~= nil
+            end,
+            blending = {
+                threshold = 0.5,
+                colorcode = '#000000',
+                hlgroup = { 'Normal', 'bg' },
+            },
+            warning = {
+                alpha = 0.4,
+                offset = 0,
+                colorcode = '#FF0000',
+                hlgroup = { 'Error', 'bg' },
+            },
+            extra = {
+                ---@type string?
+                follow_tw = nil,
+            },
+        }
+        require('deadcolumn').setup(opts)
+    end
+
+}
